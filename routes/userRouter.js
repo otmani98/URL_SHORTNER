39cc2a7038f15721ac8de821a8d2eddc6f27b474
otmani98 <otmani.ahmed.abk@gmail.com>
2023-08-14 08:01:39 +0100

    change method

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.route('/signup').post(authController.signup);
 router.route('/signup/:token').get(authController.confirmSignUp);
 router.route('/login').post(authController.login);
-router.route('/logout').post(authController.logout);
+router.route('/logout').get(authController.logout);
 router.route('/forgotPassword').post(authController.forgotPassword);
 router.route('/resetPassword/:token').patch(authController.resetPassword);
 router.route('/me').get(authController.protect, userController.getUser);
